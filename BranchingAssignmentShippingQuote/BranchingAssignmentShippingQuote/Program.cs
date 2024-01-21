cc8780126860f56e134076961fcac34b0f4fff30
JeremyMarkWilcox <jeremymarkwilcox@gmail.com>
2024-01-21 16:03:17 -0500

    Branch initial commit

---
+++ BranchingAssignmentShippingQuote/BranchingAssignmentShippingQuote/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace BranchingAssignmentShippingQuote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            // Prompt user for package weight
+            Console.Write("Please enter the weight of the package: ");
+            double weight = double.Parse(Console.ReadLine());
+
+            // Check if weight is too heavy
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                return; 
+            }
+
+            // Prompt user for package dimensions
+            Console.Write("Please enter the package width: ");
+            double width = double.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the package height: ");
+            double height = double.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the package length: ");
+            double length = double.Parse(Console.ReadLine());
+
+            // Function that Calculates the dimension's total
+            double dimensionsTotal = width + height + length;
+
+            // Check if dimensions are too big
+            if (dimensionsTotal > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.");
+                return; 
+            }
+
+            // Calculate the customers quote
+            double quote = (width * height * length * weight) / 100; //Max integer result is $2094.75
+
+            
+            Console.WriteLine($"Your estimated total for shipping this package is: ${quote:F2}");
+            Console.WriteLine("Thank you!");
+        }
+    }
+}
+
---
+++ BranchingCSharp/BranchingCSharp/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace BranchingCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What is your favorite number?");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+
+            string result = favNum == 12 ? "You have an awesome favorite number." : "You do not have an awesome favorite number.";
+            Console.WriteLine(result);
+
+
+
+
+
+
+            //int roomTemperature1 = 70;
+
+            //Console.WriteLine("Hi, what is your name?");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Hi, " + name + ", what is the temperature where you are?");
+            //int currentTemperature1 = Convert.ToInt32(Console.ReadLine());
+
+            //if (currentTemperature1 == roomTemperature1)
+            //{
+            //    Console.WriteLine("It is exactly room temperture.");
+            //}
+            //else if (currentTemperature1 > roomTemperature1)
+            //{
+            //    Console.WriteLine("It is warmer than room temperture.");
+            //}
+            //else if (currentTemperature1 < roomTemperature1)
+            //{
+            //    Console.WriteLine("It is colder than room temperture.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Something went wrong!");
+            //}
+
+
+
+            //int currentTemperature = 80;
+            //int roomTemperature = 70;
+
+            //string comparisonResult = currentTemperature == roomTemperature ?
+            //    "It is room temp. " : "It is not room temp.";
+
+            //Console.WriteLine(comparisonResult);
+
+
+            //if (currentTemperature == roomTemperature)
+            //{
+            //    Console.WriteLine("It is exactly room temperature.");
+            //}
+
+            //else if (currentTemperature > roomTemperature)
+            //    {
+            //    Console.WriteLine("It is warmer than room temperature.");
+            //}
+
+            //else if (currentTemperature < roomTemperature)
+            //{
+            //    Console.WriteLine("It is colder than room temperature.");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("It is not exactly room temperature.");
+            //}
+
+            Console.ReadLine();
+        }
+    }
+}
