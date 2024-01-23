32c1cd12ff9bc844da01f29e6a588d862b973d2b
JeremyMarkWilcox <jeremymarkwilcox@gmail.com>
2024-01-23 07:35:08 -0500

    initial commit

---
+++ AbstractAssignment/AbstractAssignment/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractAssignment
+{
+    // Derived class Employee
+    class Employee : Person
+    {
+        // Implementation of the abstract method
+        public override void SayName()
+        {
+            Console.WriteLine($"Employee Name: {FirstName} {LastName}");
+        }
+    }
+}
---
+++ AbstractAssignment/AbstractAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AbstractAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate an Employee object
+            Employee employee = new Employee
+            {
+                FirstName = "Sample",
+                LastName = "Student"
+            };
+
+            // Call the SayName() method on the object
+            employee.SayName();
+
+            // Keep the console window open
+            Console.ReadLine();
+        }
+    }
+}
