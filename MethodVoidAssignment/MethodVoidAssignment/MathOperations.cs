13bb5c19e36349c4bf6cd372727f6e256af7c927
JeremyMarkWilcox <jeremymarkwilcox@gmail.com>
2024-01-23 03:39:59 -0500

    initial commit

---
+++ MethodVoidAssignment/MethodVoidAssignment/MathOperations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodVoidAssignment
+{
+    class MathOperations
+    {
+        // Void method taking two empty integers as parameters
+        public void PerformMathOperation(int firstNumber, int secondNumber)
+        {
+            // Doing a math operation on the first integer (for example, adding 5)
+            int result = firstNumber + 5;
+
+            // Displaying the second integer to the screen
+            Console.WriteLine($"Result of the math operation: {result}, Displayed Second Number: {secondNumber}");
+        }
+    }
+}
---
+++ MethodVoidAssignment/MethodVoidAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MethodVoidAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the class
+            MathOperations mathOperations = new MathOperations();
+
+            // Call the method in the class, passing in two numbers
+            mathOperations.PerformMathOperation(10, 20);
+
+            // Call the method in the class, specifying the parameters by name
+            mathOperations.PerformMathOperation(firstNumber: 8, secondNumber: 15);
+        }
+    }
+}
