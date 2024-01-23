dcb6da5e37914cea498ffeab33dff2f74f44bb22
JeremyMarkWilcox <jeremymarkwilcox@gmail.com>
2024-01-22 20:06:25 -0500

    initial commit

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CallingMethodsAssignment
+{
+    public class MathOperations
+    {
+        public int AddOne(int num)
+        {
+            return num + 1;
+        }
+
+        public int Squared(int num)
+        {
+            return num * num;
+        }
+
+        public int Double(int num)
+        {
+            return num * 2;
+        }
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number for math operations:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            MathOperations mathOps = new MathOperations();
+
+            // Call each method and display the result
+            int resultAddOne = mathOps.AddOne(userInput);
+            Console.WriteLine($"Result after adding one: {resultAddOne}");
+
+            int resultSquared = mathOps.Squared(userInput);
+            Console.WriteLine($"Result after squaring: {resultSquared}");
+
+            int resultDouble = mathOps.Double(userInput);
+            Console.WriteLine($"Result after doubling: {resultDouble}");
+        }
+    }
+}
