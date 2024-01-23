1f9ab2ba0345ec255c96c278dc9d7e5bf449008e
JeremyMarkWilcox <jeremymarkwilcox@gmail.com>
2024-01-23 10:09:29 -0500

    initial commit

---
+++ OperatorOverloadAssignment/OperatorOverloadAssignment/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperatorOverloadAssignment
+{
+    class Employee
+    {
+        public int Id { get; set; }
+
+        
+
+        // Overloading the == operator
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            // Check if both objects are null 
+            return ReferenceEquals(employee1, null) && ReferenceEquals(employee2, null) ||
+                   //This checks if their Id properties are equal
+                   !ReferenceEquals(employee1, null) && !ReferenceEquals(employee2, null) && employee1.Id == employee2.Id;
+        }
+
+        // Overloading the != operator by using the negation of the == operator
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return !(employee1 == employee2);
+        }
+    }
+}
---
+++ OperatorOverloadAssignment/OperatorOverloadAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OperatorOverloadAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating the objects Employee 1- 3
+
+            Employee employee1 = new Employee { Id = 1 };
+            Employee employee2 = new Employee { Id = 2 };
+            Employee employee3 = new Employee { Id = 1 };
+
+            // Testing the overloaded operators by displaying it in the output.
+            // A very simple write up of them side by side for ease
+            Console.WriteLine($"employee1 == employee2: {employee1 == employee2}"); // Should be False
+            Console.WriteLine($"employee1 != employee2: {employee1 != employee2}"); // Should be True
+
+            Console.WriteLine($"employee1 == employee3: {employee1 == employee3}"); // Should be True
+            Console.WriteLine($"employee1 != employee3: {employee1 != employee3}"); // Should be False
+        }
+    }
+}
