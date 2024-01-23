b04074595409680ced210d2755f983c4a9fa1ff4
JeremyMarkWilcox <jeremymarkwilcox@gmail.com>
2024-01-23 14:32:38 -0500

    initial commit

---
+++ StructureAssignment/StructureAssignment/NumberStruct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StructureAssignment
+{
+    // Define the struct Number
+    struct Number
+    {
+        public decimal Amount { get; set; }
+
+        // Constructor to initialize the Amount property
+        public Number(decimal amount)
+        {
+            // I tried to set it without parameters the first time and
+            // from what I read it can't because a Struct is a value type, interesting!
+            Amount = amount;
+        }
+    }
+}
---
+++ StructureAssignment/StructureAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StructureAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an object of data type Number and initialize it with an amount, might as well be decimal that represents money
+            Number myNumber = new Number(42.56m);
+
+            // Print the amount to the console
+            Console.WriteLine("Amount: " + myNumber.Amount);
+        }
+    }
+}
+
