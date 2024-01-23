1cd23ac860dad5a8c27ad062bb4c4d15719f0161
JeremyMarkWilcox <jeremymarkwilcox@gmail.com>
2024-01-23 10:34:47 -0500

    initial commit

---
+++ ParametersGenericAssignment/ParametersGenericAssignment/EmployeeT.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParametersGenericAssignment
+{
+    // The T is the placeholder for the generic type parameter
+    class Employee<T>
+    {
+        // Property "Things" with a generic list matching the generic type of the class
+        public List<T> Things { get; set; } = new List<T>();
+    }
+}
---
+++ ParametersGenericAssignment/ParametersGenericAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParametersGenericAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate an Employee object with type "string"
+            Employee<string> stringEmployee = new Employee<string>();
+            stringEmployee.Things = new List<string> { "Wrench", "Hammer", "Screw Driver" };
+
+            // Instantiate an Employee object with type "int"
+            Employee<int> intEmployee = new Employee<int>();
+            intEmployee.Things = new List<int> { 1, 2, 3, 4, 5 };
+
+            // This foreach loop prints all of the Things/ Tools to the Console
+            Console.WriteLine("Tools of the Employee:");
+            foreach (var thing in stringEmployee.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            Console.WriteLine("\nThe Employee also has five unknown things:");
+            foreach (var thing in intEmployee.Things)
+            {
+                Console.WriteLine(thing);
+            }
+        }
+    }
+}
